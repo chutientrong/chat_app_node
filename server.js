2b1bd3e691dbeffb42fbf93b5cd8df8e51c750f2
chutientrong <trongct.uit@gmail.com>
2023-06-08 19:40:57 +0700

    change target socket

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = app.listen(process.env.PORT || 4000, () => {
 const io = require("socket.io")(server, {
 	pingTimeout: 60000,
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://chat-app-react-one.vercel.app",
 	},
 });
 
